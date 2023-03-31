189672c59a2fdb8532b6846b57a155d387468f3f
Benjamin Lesne <benjamin.lesne@outlook.fr>
2023-03-31 19:42:53 +0200

    config to build images

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app', './assets/app.ts')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -43,7 +43,7 @@ Encore
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+    .enableVersioning()
 
     // configure Babel
     // .configureBabel((config) => {
@@ -71,6 +71,13 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+    .copyFiles({
+         from: './assets/images',
+         // optional target path, relative to the output dir
+         to: 'images/[path][name].[hash:8].[ext]',
+         // if versioning is enabled, add the file hash too
+         //to: 'images/[path][name].[hash:8].[ext]',
+     })
 ;
 
 module.exports = Encore.getWebpackConfig();
